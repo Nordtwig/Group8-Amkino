235737f1790f78683d7ddaec40ac77a160af9851
Klutchie <43027043+Klutchie@users.noreply.github.com>
2018-11-05 15:51:01 +0100

    Added blink-Juice to map

---
+++ Amkino/Assets/BlinkingLights.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLights : MonoBehaviour {
+
+    public float Timer = 0;
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= 2)
+        {
+            
+            StartCoroutine(Blink());
+            Timer = 0;
+        }
+    }
+
+        IEnumerator Blink()
+        {
+        Light light = GetComponent<Light>();
+        light.enabled = true;
+            yield return new WaitForSeconds(0);
+        light.enabled = false;
+        }
+    
+}
