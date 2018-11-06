7fbb82df9c5a7b360f9d2521cf02a2e2f51ce050
Noah <41136053+Nordtwig@users.noreply.github.com>
2018-11-06 15:49:19 +0100

    Added player mouse rotation.

--- Amkino/Assets/Player.cs
+++ Amkino/Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour {
 
     public Camera playerCamera;
     public float moveSpeed;
+    public float rotateSpeed;
 
     private Vector3 offset;
 
@@ -19,7 +20,19 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+        float hitDist = 0.0f;
+
+        if (playerPlane.Raycast (ray, out hitDist)) {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        }
+        
+
         playerCamera.transform.position = rb.transform.position + offset;
 
         float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
