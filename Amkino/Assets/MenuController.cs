83015ce249f09362ce1ced7811b7c0a216c756d9
Noah <41136053+Nordtwig@users.noreply.github.com>
2018-11-07 10:10:23 +0100

    Added interaction to Menu.

--- Amkino/Assets/MenuController.cs
+++ Amkino/Assets/MenuController.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour {
 
+    public GameObject selection;
+
+    int currentSelect;
+    int newSelect;
+
+    [SerializeField]
+    GameObject[] buttons;
+
 	// Use this for initialization
 	void Start () {
-		
+        currentSelect = 0;
+        MoveSelection();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyUp("s")) {
+            newSelect = currentSelect + 1;
+            if (newSelect > (buttons.Length - 1)) {
+                newSelect = 0;
+            }
+            currentSelect = newSelect;
+            MoveSelection();
+        }
+        else if (Input.GetKeyUp("w")) {
+            newSelect = currentSelect - 1;
+            if (newSelect < 0) {
+                newSelect = 4;
+            }
+            currentSelect = newSelect;
+            MoveSelection();
+        }
 	}
+
+    public void MoveSelection() {
+        Vector3 newPos = new Vector3(selection.transform.position.x, selection.transform.position.y, buttons[currentSelect].transform.position.z);
+        selection.transform.position = newPos;
+    }
 }
