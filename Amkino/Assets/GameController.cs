8fa7ed3726f62a3e77e1137c0b6d5b93b8579a47
Noah <41136053+Nordtwig@users.noreply.github.com>
2018-11-08 21:17:09 +0100

    Moved Enemy and Item Spawn to GameController.

---
+++ Amkino/Assets/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Transform[] enemySpawnPoints;
+    public Transform[] itemSpawnPoints;
+
+    public GameObject EnemyPrefab;
+    public GameObject ItemPrefab;
+
+    private float enemyTimeSinceSpawn;
+    private float itemTimeSinceSpawn;
+
+    public float enemySpawnRate;
+    public float itemSpawnRate;
+	
+	// Update is called once per frame
+	void Update () {
+        enemyTimeSinceSpawn += Time.deltaTime;
+        itemTimeSinceSpawn += Time.deltaTime;
+
+        EnemySpawner();
+        ItemSpawner();
+	}
+
+    private void EnemySpawner() {
+        if (enemyTimeSinceSpawn >= enemySpawnRate) {
+            int randomIndex = UnityEngine.Random.Range(0, enemySpawnPoints.Length);
+
+
+            for (int i = 0; i < enemySpawnPoints.Length; i++) {
+                if (randomIndex == i) {
+                    Instantiate(EnemyPrefab, enemySpawnPoints[i].position, Quaternion.identity);
+                }
+            }
+
+            enemyTimeSinceSpawn = 0;
+        }
+    }
+
+    private void ItemSpawner() {
+        if (itemTimeSinceSpawn >= itemSpawnRate) {
+            int randomIndex = UnityEngine.Random.Range(0, itemSpawnPoints.Length);
+
+
+            for (int i = 0; i < itemSpawnPoints.Length; i++) {
+                if (randomIndex == i) {
+                    Instantiate(ItemPrefab, itemSpawnPoints[i].position, Quaternion.identity);
+                }
+            }
+
+            itemTimeSinceSpawn = 0;
+        }
+    }
+}
